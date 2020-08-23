19a9d70aa521340db1021ddecc0923f8cb6db949
edithpe <edita.dinet@gmail.com>
2020-08-22 22:44:57 -0500

    Updates

---
+++ precache-manifest.9cbb983441b0c1f2e3c0d4519279c0ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1b6dde6bf715189918ecd7234d63b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d330c7ac58febcdf2c68",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d330c7ac58febcdf2c68",
+    "url": "/static/js/main.d5663ad0.chunk.js"
+  },
+  {
+    "revision": "93b51166f3b499d40425",
+    "url": "/static/js/runtime-main.fbf42b67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
